0f4e8d2ee8e93a6785f9c3c1b21ed13cd507b787
MohitGupta <krishgupta1630@gmail.com>
2024-02-17 12:23:14 +0530

    initalize frontend

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-green-600 text-white">FlavorWave.com</div>
+      <Button className="m-2">Click me</Button>
+    </>
+  );
+};
+
+export default App;
