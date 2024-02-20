e5c7ecb5b519014dccec2dc2b83695151bfdae7c
MohitGupta <krishgupta1630@gmail.com>
2024-02-20 23:21:38 +0530

    added create user request

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./global.css";
 import AuthProvider from "./auth/AuthProvider.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./global.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>
 );
