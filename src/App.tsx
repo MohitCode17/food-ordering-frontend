fba0a758dc87281ade41f42b661db1c243051641
MohitGupta <krishgupta1630@gmail.com>
2024-02-24 14:24:45 +0530

    added protectedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 const App = () => {
   return (
@@ -16,14 +17,16 @@ const App = () => {
         }
       />
 
-      <Route
-        path="/user-profile"
-        element={
-          <Layout>
-            <UserProfilePage />
-          </Layout>
-        }
-      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+      </Route>
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
